c44832cfb2200842e721d6dca8a4ee9b392c1187
msmanawadu <msmanawadu@gmail.com>
2022-10-11 17:12:48 +0530

    Integrating React-Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { SiDatadog } from 'react-icons/si';
 import './App.css';
-import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <Row>
+          <Col className="mt-3">
+            <SiDatadog className="app-logo" />
+
+            <h1 className="main-heading display-3">Your Appointments</h1>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
